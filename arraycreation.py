b2135fad78205ce0705c47188a409d1d274228e7
Disha Singh <dishasingghh.com>
2026-05-29 13:16:01 +0530

    array creation

---
+++ arraycreation.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# np.array()
+a = np.array([1, 2, 3, 4, 5])
+
+print(a)
+print(a.shape)
+print(a.ndim)
+print(a.dtype)
+
+# np.zeros()
+b = np.zeros((3, 4))
+
+print(b)
+print(b.shape)
+print(b.ndim)
+print(b.dtype)
+
+# np.ones()
+c = np.ones((2, 3))
+
+print(c)
+print(c.shape)
+print(c.ndim)
+print(c.dtype)
+
